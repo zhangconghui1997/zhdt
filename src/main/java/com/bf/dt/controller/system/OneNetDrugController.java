027e6cdc8abccc2a9a793408353d424f92ddaf2b
zhangconghui1997 <376893844@qq.com>
2020-05-20 10:47:02 +0800

    文件的列表和下载

--- src/main/java/com/bf/dt/controller/system/OneNetDrugController.java
+++ src/main/java/com/bf/dt/controller/system/OneNetDrugController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 
 @RequestMapping("oneNetDrug")
@@ -31,7 +31,7 @@ public class OneNetDrugController {
         }
         String filename = file.getOriginalFilename();
         String renameFile = FileUtil.renameFile(filename);
-        String fileDirPath = new String("src/main/resources/static/img/drug");
+        String fileDirPath = new String(FileUtil.dImg);
         File fileDir = new File(fileDirPath);
         File newFile = new File(fileDir.getAbsolutePath() + File.separator + renameFile);
         try {
@@ -46,19 +46,71 @@ public class OneNetDrugController {
     @RequestMapping(value = "/dDescUpload")
     @ResponseBody
         public  MsgResult dDescUpload(MultipartFile dDesc) {
+        String s = dDesc.getOriginalFilename().substring(dDesc.getOriginalFilename().lastIndexOf('.'));
+        System.out.println(s);
         File localFile = null;
+        String descName = new Date().getTime()+s;
         try {
             if (dDesc.isEmpty()) {
                 System.out.println("文件为空");
             }
-            String folder = "f:/drug";
-            localFile = new File(folder,new Date().getTime() + ".txt");
+
+            localFile = new File(FileUtil.dDesc,descName);
             dDesc.transferTo(localFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return  MsgResult.success("200",localFile,"ok");
+        return  MsgResult.success("200",descName,"ok");
     }
+
+
+    @RequestMapping(value = "/dDescDownload")
+
+    public void dDescDownload(String descName, HttpServletResponse response) {
+        // 文件地址，真实环境是存放在数据库中的
+        String path = FileUtil.dDesc+descName;
+        File file = new File(path);
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        if (file.exists()) {
+        try {
+                response.setContentType("application/force-download");
+                // 设置下载后的文件名以及header
+                response.addHeader("Content-disposition", "attachment;fileName=" + descName);
+                byte[] buf = new byte[1024];
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                // 创建输出对象
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buf);
+                while (i != -1) {
+                    os.write(buf, 0, i);
+                    i = bis.read(buf);
+                }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        }
+
+    }
+
 
 
 
--- src/main/java/com/bf/dt/util/FileUtil.java
+++ src/main/java/com/bf/dt/util/FileUtil.java
@@ -3,6 +3,8 @@ package com.bf.dt.util;
 import java.util.UUID;
 
 public class FileUtil {
+    public static final String dDesc = "f:/drug/";
+    public static final String dImg = "src/main/resources/static/img/drug";
 
     public static String renameFile(String fileName){
         if(fileName.length()>50){
